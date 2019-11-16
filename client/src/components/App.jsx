7a5a1326e8213600c5d6ee6a14fbfe00ce4cada4
Landon <lndncole@gmail.com>
2019-11-16 14:59:04 +0700

    Set up server - GET request successful, now need to connect to index.html to serve front end files

---
+++ client/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+
+  }
+
+  render() {
+    return (
+      <div className="parent">
+        <table>
+          <tbody>
+            <h1>hello world</h1>
+          </tbody>
+        </table>
+        <button onClick={this.handleClick}>Reset Game</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+// const model = require('./models/model.js'); <-- UNCOMMENT IF I USE MODELS
+// const sControllers = require('.sdb/index.js');
+const parser = require('body-parser');
+const app = express();
+// const router = require('./routes.js'); <- MORE THAN LIKELY NOT NEEDED DUE TO ONLY HAVING ONE ENDPOINT
+
+app.use(parser.json());
+app.use(express.static('public'));
+
+// app.use('/', router);
+
+app.get('/', function (req, res) {
+  res.send('Get Request - Rendering Home Page');
+});
+
+app.listen(3000, () => {
+  console.log('Successfully Connected to the Server on port 3000');
+});
