cb7f0b4fadf93de380ac264ee957ebbf3881de3c
Landon <lndncole@gmail.com>
2019-11-22 17:07:11 +0700

    updated travis yml file to include new services that could be causing connection error - retrying GET request to localhost

--- spec/index.test.js
+++ spec/index.test.js
@@ -1,6 +1,7 @@
 const expect = require('chai').expect;
 
-const get = require('./test-server.js').get;
+const getS3 = require('./test-server.js').getS3;
+const getLocal = require('./test-server.js').getLocal;
 
 describe('First test', () => {
   it('Should assert true to be true', () => {
@@ -11,7 +12,18 @@ describe('First test', () => {
 
 describe('Access S3 server object', () => {
   it('Should return a string in response to a GET request to an object in S3', () => {
-    return get()
+    return getS3()
+      .then(response => {
+        console.log(typeof response);
+        //expect an string back
+        expect(typeof response).to.equal('string');
+      });
+  });
+});
+
+describe('Successfully hit '/' on GET request and return string', () => {
+  it('Should return a string in response to a GET request to local host', () => {
+    return getLocal()
       .then(response => {
         console.log(typeof response);
         //expect an string back
--- spec/test-server.js
+++ spec/test-server.js
@@ -1,9 +1,15 @@
 const axios = require('axios');
 
 module.exports = {
-  get() {
+  getS3() {
     return axios
       .get(`https://cap-bnb-photo-service.s3.us-east-2.amazonaws.com/1-stream-belmont-3.jpg`)
+      .then(res => res.data)
+      .catch(error => console.log(error));
+  }
+  getLocal() {
+    return axios
+      .get(`http://localhost:8080/`)
       .then(res => res.data)
       .catch(error => console.log(error));
   }
