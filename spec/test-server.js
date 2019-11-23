baffdbc86e4baab2e687943eb068362adcf156cb
Landon <lndncole@gmail.com>
2019-11-23 18:58:54 +0700

    updated script file in package.JSON to inclue --exit command to test with Travis CI

--- spec/test-server.js
+++ spec/test-server.js
@@ -1,10 +1,10 @@
-const axios = require('axios');
-
-module.exports = {
-  getResponseS3() {
-    return axios
-      .get(`https://cap-bnb-photo-service.s3.us-east-2.amazonaws.com/1-stream-belmont-3.jpg`)
-      .then(res => res.data)
-      .catch(error => console.log(error));
-  }
-};
+// const axios = require('axios');
+//
+// module.exports = {
+//   getResponseS3() {
+//     return axios
+//       .get(`https://cap-bnb-photo-service.s3.us-east-2.amazonaws.com/1-stream-belmont-3.jpg`)
+//       .then(res => res.data)
+//       .catch(error => console.log(error));
+//   }
+// };
