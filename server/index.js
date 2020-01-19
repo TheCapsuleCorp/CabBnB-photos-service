559fb7692e0fec738f1856b40f80eb5846d738ca
Landon <lndncole@gmail.com>
2020-01-18 21:46:48 -0500

    steve convo

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const db = require('../db/index.js');
 
 const PORT = process.env.PORT || 8080;
 
+db.connect();
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
-
-db.connect();
